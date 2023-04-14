39cc766f9b09eda63faf9289520a88f90f138ad7
[hsb031106@naver.com] <hsb031106@naver.com>
2023-04-15 02:13:07 +0900

    230415

--- 5764263/Assets/Scripts/PlayerCtrl.cs
+++ 5764263/Assets/Scripts/PlayerCtrl.cs
@@ -1,24 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
+
 
 public class PlayerCtrl : MonoBehaviour
 {
-    int speed = 2;
+    int moveSpeed = 5;
 
     Rigidbody2D rigidBody;
     Vector2 vector;
 
+    int HP;
+
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
+        this.rigidBody = GetComponent<Rigidbody2D>();
+        this.HP = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMove();
+        Debug.Log(this.HP);
     }
 
     void PlayerMove()
@@ -26,6 +33,12 @@ public class PlayerCtrl : MonoBehaviour
         vector.x = Input.GetAxisRaw("Horizontal");
         vector.y = Input.GetAxisRaw("Vertical");
 
-        rigidBody.velocity = vector * speed;
+        rigidBody.velocity = vector * moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
     }
 }
+
