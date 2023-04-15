5126bdf6d24582795581edbbc095dc972e4bd53a
[hsb031106@naver.com] <hsb031106@naver.com>
2023-04-16 00:36:18 +0900

    230416

---
+++ 5764263/Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private const float moveSpeed = 10.0f; // 총알 속도
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        // 총알 발사
+        float moveY = moveSpeed * Time.deltaTime; // 스피드(Y축으로 이동함)
+        transform.Translate(0, moveY, 0); // 이동
+
+        Destroy(gameObject, 2.0f); // 발사되고 2초 뒤 삭제
+    }
+
+    // 적 충돌 처리
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 적과 부딪히면 총알, 적 오브젝트 삭제
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("collision");
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+
+        }
+
+    }
+}
--- 5764263/Assets/Scripts/PlayerCtrl.cs
+++ 5764263/Assets/Scripts/PlayerCtrl.cs
@@ -7,38 +7,88 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    int moveSpeed = 5;
-
     Rigidbody2D rigidBody;
     Vector2 vector;
+    Transform Player;
 
-    int HP;
+    public const float moveSpeed = 5.0f;
+
+    public GameObject firePrefab;
+    GameObject fireLoc;
+
+    bool canFire = true;
+    const float fireDelay = 0.5f; // 총알 발사 딜레이 시간(쿨타임)
+    float fireTimer = 0; // 총알 발사 후 시간 세기
+
 
     // Start is called before the first frame update
     void Start()
     {
         this.rigidBody = GetComponent<Rigidbody2D>();
-        this.HP = 100;
+        this.Player = GetComponent<Transform>();
+        this.fireLoc = GameObject.Find("FireLocation");
+   
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMove();
-        Debug.Log(this.HP);
+        FireControl();
+        PlayerRotation();
+
     }
 
+    // 플레이어 이동
     void PlayerMove()
     {
-        vector.x = Input.GetAxisRaw("Horizontal");
-        vector.y = Input.GetAxisRaw("Vertical");
+        vector.x = Input.GetAxisRaw("Horizontal"); // 좌우 이동(A, D키)
+        vector.y = Input.GetAxisRaw("Vertical"); // 상하 이동(W, S키)
 
-        rigidBody.velocity = vector * moveSpeed;
+        rigidBody.velocity = vector * moveSpeed; // 플레이어 이동
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    // 플레이어 각도 변환
+    void PlayerRotation()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Player.transform.localEulerAngles = new Vector3(0, 0, 180);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Player.transform.localEulerAngles = new Vector3(0, 0, 0);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Player.transform.localEulerAngles = new Vector3(0, 0, 90);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Player.transform.localEulerAngles = new Vector3(0, 0, 270);
+        }
+    }
+
+    // 총 발사
+    void FireControl()
+    {
+        if (canFire)
+        {
+            if (fireTimer > fireDelay)
+            {
+                // 클릭하면 총알 생성
+                if (Input.GetMouseButtonDown(0))
+                    Instantiate(firePrefab, fireLoc.transform.position, fireLoc.transform.rotation);
+            }
+
+
+            fireTimer += Time.deltaTime;
+        }
     }
 }
+
 
